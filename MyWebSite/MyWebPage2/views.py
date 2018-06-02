cdd3ef3062712e4df242f8f129b86ddfe070d686
ermilovim <ermilovim@gmail.com>
2018-06-02 13:41:56 +0300

    addind db!
    have added database to site!!!
    (/table)

--- MyWebSite/MyWebPage2/views.py
+++ MyWebSite/MyWebPage2/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, render_to_response
+import sqlite3
 # Create your views here.
 
 def index(request):
-    return render(request,'mywebpage2/index.html')
+    # логика работы с данными
+    con = sqlite3.connect('user.db')
+    cur = con.cursor()
+    data = cur.execute('SELECT * FROM user')
+    # упаковываешь контекст
+    context = {'table_in_html': data}
+    return render_to_response("mywebpage2/main.html", context)
