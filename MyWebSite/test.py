cf40cb5216fe5b733d460defd3ea07877f49289f
ermilovim <ermilovim@gmail.com>
2018-07-09 23:20:19 +0300

    some new adds
    добавил переход с главной страницы на таблицы и кнопку назад

--- MyWebSite/test.py
+++ MyWebSite/test.py
@@ -6,20 +6,20 @@ import sys
 
 
 def check_exist_user_by_id():
-    con = sqlite3.connect('user.db')
+    con = sqlite3.connect('users.db')
     cur = con.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS user (id VARCHAR(100), existence VARCHAR(15))')
+    cur.execute('CREATE TABLE IF NOT EXISTS users (id VARCHAR(100), existence VARCHAR(15))')
     con.commit()
     ids = open("ids", 'r').readlines()
     for id in ids:
         id = id.strip()
-        cur.execute("insert into user values (?, null)", [id])
+        cur.execute("insert into users values (?, null)", [id])
     con.commit()
 
     with open("ids", 'w') as f:
         f.write('')
 
-    ids = cur.execute('SELECT id FROM user where existence is null').fetchall()
+    ids = cur.execute('SELECT id FROM users where existence is null').fetchall()
 
     graph = facebook.GraphAPI('1938851329475559|9N7KF0F4LokFujG_oTibHwz3YwM')
 
@@ -39,7 +39,7 @@ def check_exist_user_by_id():
             else:
                 existence = True
         existence = 'Exists' if existence else 'Not exists'
-        cur.execute('update user set existence = "{}" where id == "{}"'.format(existence, id))
+        cur.execute('update users set existence = "{}" where id == "{}"'.format(existence, id))
     con.commit()
     return
 
